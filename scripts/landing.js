1693e7edebc856767d564f4f4d473f4cdbf39524
Leah <leahstuckey@me.com>
2017-07-06 12:51:22 -0500

    wip

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,6 @@
-var pointsArray = document.getElementsByClassName('point');
+ var animatePoints = function() {
 
-var animatePoints = function(points) {
+     var points = document.getElementsByClassName('point');
 
      var revealPoint = function(index) {
          points[index].style.opacity = 1;
@@ -14,17 +14,13 @@ var animatePoints = function(points) {
  };
 
  window.onload = function() {
+   if (window.innerHeight > 950) {
+     animatePoints(pointsArray);
+   }
 
-      if (window.innerHeight > 950) {
-        animatePoints(pointsArray);
-      }
-
-      var sellingPoints = document.getElementsByClassName('selling-points')[0];
-      var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-
-      window.addEventListener('scroll', function(event) {
-        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-             animatePoints(pointsArray);
-         }
-     });
-  }
+   window.addEventListener("scroll", function(event) {
+     if (pointsArray[0].getBoundingClientRect().top <= 500) {
+       animatePoints(pointsArray);
+     }
+   });
+ }
