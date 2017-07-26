6a8c0833f8d5ee158a704d58bb57915a7669feaf
Leah <leahstuckey@me.com>
2017-07-26 11:28:29 -0500

    Checkpoint-24-work-

--- scripts/album.js
+++ scripts/album.js
@@ -13,7 +13,6 @@ var albumPicasso = {
      ]
  };
 
- // Another Example Album
  var albumMarconi = {
      title: 'The Telephone',
      artist: 'Guglielmo Marconi',
@@ -40,3 +39,40 @@ var albumPicasso = {
 
      return template;
  };
+
+
+
+var albumTitle = document.getElementsByClassName('album-view-title')[0];
+var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
+var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+
+var setCurrentAlbum = function(album) {
+     albumTitle.firstChild.nodeValue = album.title;
+     albumArtist.firstChild.nodeValue = album.artist;
+     albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
+     albumImage.setAttribute('src', album.albumArtUrl);
+
+     albumSongList.innerHTML = '';
+
+
+     for (var i = 0; i < album.songs.length; i++) {
+         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+     }
+ };
+
+ window.onload = function() {
+     setCurrentAlbum(albumPicasso);
+ };
+
+ var albums = [albumPicasso, albumMarconi, albumPrestigeWorldWide];
+ var index = 0;
+ albumImage.addEventListener("click", function(event){
+   setCurrentAlbum(albums[index]);
+   index++;
+   if (index == albums.length){
+     index = 0;
+   }
+ });
+};
