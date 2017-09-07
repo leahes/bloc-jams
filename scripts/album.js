3ed2c90347220bedff52a10f80b18c5466375ff9
Leah <leahstuckey@me.com>
2017-09-06 19:42:36 -0500

    wip 32-assign

--- scripts/album.js
+++ scripts/album.js
@@ -9,18 +9,26 @@
 
      var $row = $(template);
 
+     var setSong = function(songNumber) {
+       currentlyPlayingSongNumber = parseInt(songNumber);
+       currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+     };
+
+     var getSongNumberCell = function(number) {
+       return $('.song-item-number[data-song-number = "'+ number +'"]')
+     };
+
      var clickHandler = function() {
        var songNumber = parseInt($(this).attr('data-song-number'));
 
           if (currentlyPlayingSongNumber !== null) {
-            var currentlyPlayingCell = $('.song-item-number[data-song-number="' + currentlyPlayingSongNumber + '"]');
+            var currentlyPlayingCell = getSongNumberCell(currentlyPlayingSongNumber);
             currentlyPlayingCell.html(currentlyPlayingSongNumber);
           }
+          
           if (currentlyPlayingSongNumber !== songNumber) {
-
             $(this).html(pauseButtonTemplate);
-            currentlyPlayingSongNumber = songNumber;
-            currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+            setSong.(songNumber);
             updatePlayerBarSong();
 
           } else if (currentlyPlayingSongNumber === songNumber) {
